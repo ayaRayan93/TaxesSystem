3a2c4e3b353802a343e161fa8b3ca57cd7705f5a
Sara Hany <sarahany595@gmail.com>
2019-02-04 16:52:52 +0200

    print storageReturn

---
+++ MainSystem/Store/Import/استرجاع/StorageReturn_Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainSystem
+{
+    public class StorageReturn_Items
+    {
+        public string Code { get; set; }
+        public string Product_Name { get; set; }
+        public double Balatat { get; set; }
+        public double Carton_Balata { get; set; }
+        public double Total_Meters { get; set; }
+        public int Supplier_Permission_Number { get; set; }
+        public string Date { get; set; }
+        public string Reason { get; set; }
+    }
+}
