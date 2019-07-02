0f995b370dfb794bb56afac233fe380d8a513627
aya ali <aya.rayan1011@gmail.com>
2019-07-02 16:19:14 +0200

    edit

--- MainSystem/Store/Storage/StorageTaswayaSubtract.cs
+++ MainSystem/Store/Storage/StorageTaswayaSubtract.cs
@@ -316,9 +316,9 @@ namespace MainSystem
                             com = new MySqlCommand(query, dbconnection);
                             if (com.ExecuteScalar() != null)
                             {
-                                Name = (string)com.ExecuteScalar();
-                                comType.Text = Name;
-                                txtType.Text = txtCodePart1.Text;
+                                //Name = (string)com.ExecuteScalar();
+                                //comType.Text = Name;
+                                //txtType.Text = txtCodePart1.Text;
                                 makeCode(txtBox);
                                 txtCodePart2.Focus();
                                 dbconnection.Close();
@@ -335,9 +335,9 @@ namespace MainSystem
                             com = new MySqlCommand(query, dbconnection);
                             if (com.ExecuteScalar() != null)
                             {
-                                Name = (string)com.ExecuteScalar();
-                                comFactory.Text = Name;
-                                txtFactory.Text = txtCodePart2.Text;
+                                //Name = (string)com.ExecuteScalar();
+                                //comFactory.Text = Name;
+                                //txtFactory.Text = txtCodePart2.Text;
                                 makeCode(txtBox);
                                 txtCodePart3.Focus();
                                 dbconnection.Close();
@@ -354,9 +354,9 @@ namespace MainSystem
                             com = new MySqlCommand(query, dbconnection);
                             if (com.ExecuteScalar() != null)
                             {
-                                Name = (string)com.ExecuteScalar();
-                                comGroup.Text = Name;
-                                txtGroup.Text = txtCodePart3.Text;
+                                //Name = (string)com.ExecuteScalar();
+                                //comGroup.Text = Name;
+                                //txtGroup.Text = txtCodePart3.Text;
                                 makeCode(txtBox);
                                 txtCodePart4.Focus();
                                 dbconnection.Close();
@@ -373,9 +373,9 @@ namespace MainSystem
                             com = new MySqlCommand(query, dbconnection);
                             if (com.ExecuteScalar() != null)
                             {
-                                Name = (string)com.ExecuteScalar();
-                                comProduct.Text = Name;
-                                txtProduct.Text = txtCodePart4.Text;
+                                //Name = (string)com.ExecuteScalar();
+                                //comProduct.Text = Name;
+                                //txtProduct.Text = txtCodePart4.Text;
                                 makeCode(txtBox);
                                 txtCodePart5.Focus();
                                 dbconnection.Close();
