8f466dfbf9fe6b3df878f47f9af39344cf63f389
Sara Hany <sarahany595@gmail.com>
2019-07-21 16:30:41 +0200

    print itemTransitions

---
+++ MainSystem/Store/Reports/تقرير حركة صنف/Print_Item_Transitions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+using System.Collections.Generic;
+
+namespace MainSystem
+{
+    public partial class Print_Item_Transitions : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Print_Item_Transitions()
+        {
+            InitializeComponent();
+        }
+
+        public void InitData(string Store, DateTime fromDate, DateTime toDate, string productName, double TotalBills, double TotalReturn, double safy, List<Item_Transitions> Bill_Items)
+        {
+            Store_Name.Value = Store;
+            FromDate.Value = fromDate;
+            ToDate.Value = toDate;
+            ProductName.Value = productName;
+            TotalIncrease.Value = TotalBills;
+            TotalDecrease.Value = TotalReturn;
+            Safy.Value = safy;
+            DateNow.Value = DateTime.Now;
+            objectDataSource1.DataSource = Bill_Items;
+        }
+    }
+}
