abec72be9fd49b5981a56f1d7f1e16d29a15b529
aya ali <aya.rayan1011@gmail.com>
2019-07-28 12:14:36 +0200

    edit

--- MainSystem/PointOfSale/InformationProducts_Report.cs
+++ MainSystem/PointOfSale/InformationProducts_Report.cs
@@ -147,17 +147,8 @@ namespace MainSystem
         {
             try
             {
-                if (comType.Text != "" && comFactory.Text != "")
+                if (comType.Text != "" && comFactory.Text != "" && comGroup.Text != "")
                 {
-                    if(comType.Text == "سيراميك" || comType.Text == "بورسلين")
-                    {
-                        if(comGroup.Text == "")
-                        {
-                            gridControl1.DataSource = null;
-                            MessageBox.Show("يجب اختيار النوع والمصنع والمجموعة على الاقل");
-                            return;
-                        }
-                    }
                     string q1, q2, q3, q4, fQuery = "";
                     if (comType.Text == "")
                     {
@@ -253,7 +244,7 @@ namespace MainSystem
                 else
                 {
                     gridControl1.DataSource = null;
-                    MessageBox.Show("يجب اختيار النوع والمصنع على الاقل");
+                    MessageBox.Show("يجب اختيار النوع والمصنع والمجموعة على الاقل");
                 }
             }
             catch (Exception ex)
