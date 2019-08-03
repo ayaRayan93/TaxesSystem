257a3bb82eaf8e512c1a0fd101d33b3284f68b1f
Sara Hany <sarahany595@gmail.com>
2019-08-03 12:10:29 +0200

    edit ticket

---
+++ MainSystem/Store/Ticket/Print_Product_Ticket_Copy4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+using System.Collections.Generic;
+
+namespace MainSystem
+{
+    public partial class Print_Product_Ticket_Copy4 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Print_Product_Ticket_Copy4()
+        {
+            InitializeComponent();
+        }
+
+        public void InitData(List<Ticket_Items> Bill_Items)
+        {
+            objectDataSource1.DataSource = Bill_Items;
+        }
+    }
+}
