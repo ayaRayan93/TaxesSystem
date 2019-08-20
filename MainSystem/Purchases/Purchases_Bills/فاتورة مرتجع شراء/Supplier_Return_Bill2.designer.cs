286b8edbe793d6760b261dc1a18dd04cfc576872
Sara Hany <sarahany595@gmail.com>
2019-08-20 17:20:05 +0200

    edit

--- MainSystem/Purchases/Purchases_Bills/فاتورة مرتجع شراء/Supplier_Return_Bill2.designer.cs
+++ MainSystem/Purchases/Purchases_Bills/فاتورة مرتجع شراء/Supplier_Return_Bill2.designer.cs
@@ -595,7 +595,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.labelSupPerm);
             this.panel1.Controls.Add(this.comSupPerm);
             this.panel1.Controls.Add(this.comSupplier);
