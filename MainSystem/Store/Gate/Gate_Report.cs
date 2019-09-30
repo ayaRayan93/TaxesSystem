77d91f54e15601082298c561cde96e7435048e0b
Sara Hany <sarahany595@gmail.com>
2019-09-30 16:02:08 +0200

    gateReport

--- MainSystem/Store/Gate/Gate_Report.cs
+++ MainSystem/Store/Gate/Gate_Report.cs
@@ -116,17 +116,21 @@ namespace MainSystem
 
             DataSet sourceDataSet = new DataSet();
             //,employee.Employee_Name as 'مسئول التعتيق',gate.Responsible as 'المسئول'
-            MySqlDataAdapter adapterZone = new MySqlDataAdapter("SELECT gate.Permission_Number as 'التسلسل',gate.Reason as 'سبب الدخول',gate.Type as 'النوع',gate.Date_Enter as 'وقت الدخول',gate.Date_Out as 'وقت الخروج',gate.Driver_Name as 'السواق',gate.Car_Number as 'رقم العربية',gate.License_Number as 'رقم الرخصة',gate.Description as 'البيان' FROM gate LEFT JOIN employee ON employee.Employee_ID = gate.TatiqEmp_ID WHERE gate.Store_ID =" + storeId + " and (gate.Date_Enter between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd HH:mm:ss") + "' or gate.Date_Out between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd HH:mm:ss") + "')", conn);
+            MySqlDataAdapter adapterGat = new MySqlDataAdapter("SELECT gate.Gate_ID as 'التسلسل',gate.Reason as 'سبب الدخول',gate.Type as 'النوع',gate.Date_Enter as 'وقت الدخول',gate.Date_Out as 'وقت الخروج',gate.Driver_Name as 'السواق',gate.Car_Number as 'رقم العربية',gate.License_Number as 'رقم الرخصة',gate.Description as 'البيان' FROM gate INNER JOIN gate_supplier ON gate.Gate_ID = gate_supplier.Gate_ID INNER JOIN gate_permission ON  gate_supplier.GateSupplier_ID = gate_permission.GateSupplier_ID LEFT JOIN employee ON employee.Employee_ID = gate.TatiqEmp_ID WHERE gate.Store_ID =" + storeId + " and (date(gate.Date_Enter) between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "' or date(gate.Date_Out) between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "')", conn);
+            MySqlDataAdapter adapterSup = new MySqlDataAdapter("SELECT gate_supplier.Gate_ID as 'التسلسل',supplier.Supplier_Name as 'المورد',gate_supplier.Type as 'النوع',gate_supplier.GateSupplier_ID as 'ID' FROM gate INNER JOIN gate_supplier ON gate.Gate_ID = gate_supplier.Gate_ID INNER JOIN supplier ON supplier.Supplier_ID = gate_supplier.Supplier_ID INNER JOIN gate_permission ON  gate_supplier.GateSupplier_ID = gate_permission.GateSupplier_ID where gate.Store_ID =" + storeId + " and (date(gate.Date_Enter) between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "' or date(gate.Date_Out) between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "')", conn);
+            MySqlDataAdapter adapterPerm = new MySqlDataAdapter("SELECT gate_permission.GateSupplier_ID as 'التسلسل',gate_permission.Supplier_PermissionNumber as 'رقم الاذن',gate_permission.Type as 'النوع',gate_permission.Permission_Image as 'الصورة' FROM gate INNER JOIN gate_supplier ON gate.Gate_ID = gate_supplier.Gate_ID INNER JOIN gate_permission ON  gate_supplier.GateSupplier_ID = gate_permission.GateSupplier_ID where gate.Store_ID =" + storeId + " and (date(gate.Date_Enter) between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "' or date(gate.Date_Out) between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "')", conn);
 
-            MySqlDataAdapter adapterArea = new MySqlDataAdapter("SELECT gate_permission.Permission_Number as 'التسلسل',gate_permission.Supplier_PermissionNumber as 'رقم الاذن',gate_permission.Type as 'النوع',gate_permission.Permission_Image as 'الصورة' FROM gate_permission inner join gate on gate_permission.Permission_Number=gate.Permission_Number where gate.Store_ID =" + storeId + " and (gate.Date_Enter between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd HH:mm:ss") + "' or gate.Date_Out between '" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and '" + dateTimePicker2.Value.ToString("yyyy-MM-dd HH:mm:ss") + "')", conn);
-
-            adapterZone.Fill(sourceDataSet, "gate");
-            adapterArea.Fill(sourceDataSet, "gate_permission");
+            adapterGat.Fill(sourceDataSet, "gate");
+            adapterSup.Fill(sourceDataSet, "gate_supplier");
+            adapterPerm.Fill(sourceDataSet, "gate_permission");
 
             //Set up a master-detail relationship between the DataTables 
             DataColumn keyColumn = sourceDataSet.Tables["gate"].Columns["التسلسل"];
-            DataColumn foreignKeyColumn = sourceDataSet.Tables["gate_permission"].Columns["التسلسل"];
-            sourceDataSet.Relations.Add("ارقام الاذون", keyColumn, foreignKeyColumn);
+            DataColumn foreignKeyColumn2 = sourceDataSet.Tables["gate_supplier"].Columns["التسلسل"];
+            DataColumn foreignKeyColumn3 = sourceDataSet.Tables["gate_supplier"].Columns["ID"];
+            DataColumn foreignKeyColumn4 = sourceDataSet.Tables["gate_permission"].Columns["التسلسل"];
+            sourceDataSet.Relations.Add("ارقام الاذون", keyColumn, foreignKeyColumn2);
+            sourceDataSet.Relations.Add("تفاصيل الاذون", foreignKeyColumn3, foreignKeyColumn4);
 
             gridControl1.DataSource = sourceDataSet.Tables["gate"];
         }
