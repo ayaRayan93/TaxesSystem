a471f8c3aa5957ba149d3844d01d64458cab9e6c
Sara Hany <sarahany595@gmail.com>
2019-11-19 13:26:04 +0200

    factoryTransitionReport and its print

---
+++ MainSystem/Store/Reports/تقرير اصناف شركة/Report_Items_Factory2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace MainSystem
+{
+    public partial class Report_Items_Factory2 : DevExpress.XtraEditors.XtraForm
+    {
+        public Report_Items_Factory2()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintInvoice(DateTime date, double TotalBillPriceAD, List<Items_Factory> BillItems)
+        {
+            Print_Items_Factory2 report = new Print_Items_Factory2();
+            foreach(DevExpress.XtraReports.Parameters.Parameter p in report.Parameters)
+            {
+                p.Visible = false;
+            }
+            report.InitData(date, TotalBillPriceAD, BillItems);
+            documentViewer1.DocumentSource = report;
+            report.CreateDocument();
+        }
+    }
+}
