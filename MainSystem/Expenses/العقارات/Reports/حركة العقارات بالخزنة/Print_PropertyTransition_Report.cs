6ce51b93211ecb6a60924a26d36256f53d88e516
aya ali <aya.rayan1011@gmail.com>
2020-04-28 13:36:51 +0200

    العقارات

---
+++ MainSystem/Expenses/العقارات/Reports/حركة العقارات بالخزنة/Print_PropertyTransition_Report.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace MainSystem
+{
+    public partial class Print_PropertyTransition_Report : DevExpress.XtraEditors.XtraForm
+    {
+        public Print_PropertyTransition_Report()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintInvoice(DateTime fromDate, DateTime toDate, string branch_Name, List<ExpensesTransition_Items> BillItems)
+        {
+            Print_PropertyTransition report = new Print_PropertyTransition();
+            foreach(DevExpress.XtraReports.Parameters.Parameter p in report.Parameters)
+            {
+                p.Visible = false;
+            }
+            report.InitData(fromDate, toDate, branch_Name, BillItems);
+            documentViewer1.DocumentSource = report;
+            report.CreateDocument();
+        }
+    }
+}
