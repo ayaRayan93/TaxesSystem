a7df046da1dcf2e7f3a385ec41a8861340d3bf8e
aya ali <aya.rayan1011@gmail.com>
2020-06-06 11:27:34 +0200

    edit complain record

---
+++ MainSystem/CustomerService/CustomerComplain/Form2ComplainDetails.designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CustomerServiceSearchPhone
+{
+    partial class Form2ComplainDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCustomerPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCustomerAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtComplain = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label10.Location = new System.Drawing.Point(221, 45);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 17);
+            this.label10.TabIndex = 100;
+            this.label10.Text = "التلفون";
+            // 
+            // txtCustomerPhone
+            // 
+            this.txtCustomerPhone.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.txtCustomerPhone.Location = new System.Drawing.Point(60, 39);
+            this.txtCustomerPhone.Name = "txtCustomerPhone";
+            this.txtCustomerPhone.ReadOnly = true;
+            this.txtCustomerPhone.Size = new System.Drawing.Size(147, 24);
+            this.txtCustomerPhone.TabIndex = 99;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label4.Location = new System.Drawing.Point(556, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 98;
+            this.label4.Text = "اسم العميل";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.txtCustomerName.Location = new System.Drawing.Point(367, 38);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.ReadOnly = true;
+            this.txtCustomerName.Size = new System.Drawing.Size(175, 24);
+            this.txtCustomerName.TabIndex = 97;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label7.Location = new System.Drawing.Point(556, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 17);
+            this.label7.TabIndex = 96;
+            this.label7.Text = "العنوان";
+            // 
+            // txtCustomerAddress
+            // 
+            this.txtCustomerAddress.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.txtCustomerAddress.Location = new System.Drawing.Point(367, 73);
+            this.txtCustomerAddress.Name = "txtCustomerAddress";
+            this.txtCustomerAddress.ReadOnly = true;
+            this.txtCustomerAddress.Size = new System.Drawing.Size(175, 24);
+            this.txtCustomerAddress.TabIndex = 95;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label1.Location = new System.Drawing.Point(447, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 102;
+            this.label1.Text = "اسم الموظف";
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.txtEmployeeName.Location = new System.Drawing.Point(258, 29);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.ReadOnly = true;
+            this.txtEmployeeName.Size = new System.Drawing.Size(175, 24);
+            this.txtEmployeeName.TabIndex = 101;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCustomerAddress);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtCustomerPhone);
+            this.groupBox1.Controls.Add(this.txtCustomerName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.groupBox1.Location = new System.Drawing.Point(58, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(662, 132);
+            this.groupBox1.TabIndex = 103;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "بيانات العميل";
+            // 
+            // txtComplain
+            // 
+            this.txtComplain.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.txtComplain.Location = new System.Drawing.Point(106, 243);
+            this.txtComplain.Multiline = true;
+            this.txtComplain.Name = "txtComplain";
+            this.txtComplain.ReadOnly = true;
+            this.txtComplain.Size = new System.Drawing.Size(534, 190);
+            this.txtComplain.TabIndex = 101;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label2.Location = new System.Drawing.Point(646, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 102;
+            this.label2.Text = "نص الشكوة";
+            // 
+            // Form2ComplainDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 476);
+            this.Controls.Add(this.txtComplain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmployeeName);
+            this.MaximizeBox = false;
+            this.Name = "Form2ComplainDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "تفاصيل الشكوى";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2ComplainDetails_FormClosed);
+            this.Load += new System.EventHandler(this.Form2ComplainDetails_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCustomerPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCustomerAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtComplain;
+        private System.Windows.Forms.Label label2;
+    }
+}
